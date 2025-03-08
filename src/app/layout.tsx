c0b8d40078c38ea08d1d7fc7f33ff3ed5abe4003
Adrian Rangel <adrianrangelargu@gmail.com>
2025-03-08 11:55:02 -0400

    setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Montserrat, Hind } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+})
+
+const hind = Hind({
+  variable: "--font-hind",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+})
+
+export const metadata: Metadata = {
+  title: "Prettyai Colors",
+  description: "Generate colors palettes, visualize on website and save them for use in your projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} ${hind.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="font-bold text-red-600 font-montserrat">Hello World</h1>
+  );
+}

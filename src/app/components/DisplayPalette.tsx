f261304ee2f6e19b28a44f616c643701f871921d
Adrian Rangel <adrianrangelargu@gmail.com>
2025-03-09 15:19:06 -0400

    lib: added daisyui for components feat: added function to copy palette and colors test: add test for function

--- src/app/components/DisplayPalette.tsx
+++ src/app/components/DisplayPalette.tsx
@@ -6,15 +6,19 @@ import { copyToClipboard } from "@/utils/copyToClipboard"
 export const DisplayPalette = () => {
   const colors = useColorStore((state) => state.colors)
   
-  if (colors.length === 0) return null
+  if (colors.length === 0) return (
+    <h3 className="text-lg font-semibold mt-12">Select a mode for generate the palette.</h3>
+  )
 
   return (
     <section className="mt-6 flex flex-col">
       <div className="flex items-center">
         {
           colors.map((color, index) => (
-            <div key={index} onClick={() => copyToClipboard(color, 'Color copied!')} className="relative w-24 h-24 border-[2px] border-transparent transition-all duration-200 hover:border-slate-800 cursor-pointer" style={{ backgroundColor: color }}>
-              <span className="absolute -bottom-6 text-sm font-semibold">{color}</span>
+            <div key={index} className="tooltip" data-tip="Copy">
+              <div key={index} onClick={() => copyToClipboard(color, 'Color copied!')} className="relative w-24 h-24 border-[2px] border-transparent transition-all duration-200 hover:border-slate-800 cursor-pointer" style={{ backgroundColor: color }}>
+                <span className="absolute -bottom-6 text-sm font-semibold">{color}</span>
+              </div>
             </div>
           ))
         }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
         <h1 className="font-montserrat text-3xl font-bold text-slate-900">PrettyaiColors</h1>
         <p className="font-semibold">Generate color palettes and visualize them on real time.</p>
       </section>
-      <section className="w-full flex flex-col lg:flex-row items-center lg:items-start space-y-4 lg:space-y-0 lg:space-x-4">
-        <div className="w-full md:w-1/2 p-4 rounded-md border-[1px] border-slate-300">
+      <section className="w-full h-[320px] flex flex-col lg:flex-row items-center lg:items-start space-y-4 lg:space-y-0 lg:space-x-4">
+        <div className="w-full md:w-9/12 lg:w-1/2 p-4 rounded-md border-[1px] border-slate-300 h-full">
           <h2 className="font-montserrat text-lg font-bold">Generate the palette</h2>
           {/* Inputs */}
           <div className="flex items-center space-x-4 mt-6">
@@ -19,7 +19,7 @@ export default function Home() {
           </div>
           <DisplayPalette />
         </div>
-        <div className="w-full md:w-1/2 p-4 rounded-md border-[1px] border-slate-300">
+        <div className="w-full md:w-9/12 lg:w-1/2 p-4 rounded-md border-[1px] border-slate-300 h-full">
           <h2 className="font-montserrat text-lg font-bold">Saved Palettes</h2>
         </div>
       </section>

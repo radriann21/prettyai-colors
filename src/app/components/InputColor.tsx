fd6ebd2a5d222cd8b922219c75e650f792095e5f
Adrian Rangel <adrianrangelargu@gmail.com>
2025-03-09 12:39:28 -0400

    feat:added basic responsive styles

--- src/app/components/InputColor.tsx
+++ src/app/components/InputColor.tsx
@@ -11,7 +11,7 @@ export const InputColor = () => {
   return (
     <label htmlFor="color" className="flex items-center space-x-1 border-[1px] rounded-md border-slate-400 p-1">
       <input type="color" id="color" value={color} onChange={(evt) => setColor(evt.target.value)} />
-      <span className="font-semibold">Choose a color</span>
+      <span className="font-semibold text-sm">Choose a color</span>
     </label>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { DisplayPalette } from "./components/DisplayPalette";
 
 export default function Home() {
   return (
-    <main className="max-w-6xl h-screen mx-auto flex flex-col py-8 relative font-hind">
-      <section className="mb-8">
+    <main className="max-w-6xl h-screen mx-auto flex flex-col px-4 xl:px-0 py-8 relative font-hind">
+      <section className="mb-8 md:text-center lg:text-left">
         <h1 className="font-montserrat text-3xl font-bold text-slate-900">PrettyaiColors</h1>
         <p className="font-semibold">Generate color palettes and visualize them on real time.</p>
       </section>
-      <section className="w-full flex space-x-4">
-        <div className="w-1/2 p-4 rounded-md border-[1px] border-slate-300">
+      <section className="w-full flex flex-col lg:flex-row items-center lg:items-start space-y-4 lg:space-y-0 lg:space-x-4">
+        <div className="w-full md:w-1/2 p-4 rounded-md border-[1px] border-slate-300">
           <h2 className="font-montserrat text-lg font-bold">Generate the palette</h2>
           {/* Inputs */}
           <div className="flex items-center space-x-4 mt-6">
@@ -17,7 +17,7 @@ export default function Home() {
           </div>
           <DisplayPalette />
         </div>
-        <div className="w-1/2 p-4 rounded-md border-[1px] border-slate-300">
+        <div className="w-full md:w-1/2 p-4 rounded-md border-[1px] border-slate-300">
           <h2 className="font-montserrat text-lg font-bold">Saved Palettes</h2>
         </div>
       </section>
